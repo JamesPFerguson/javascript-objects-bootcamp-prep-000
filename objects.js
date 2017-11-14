7e137912982e18a322af9cca991a408bf907927e
James Ferguson <ncjamesferguson@gmail.com>
2017-11-14 16:57:09 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,11 +4,11 @@ var playlist = {
   Yeasayer: 'Sunrise'
 }
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist[artistName] = [songTitle]
+  playlist[artistName] = songTitle
   return playlist
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist.artistName;
+  delete playlist[artistName];
   return playlist;
 }
