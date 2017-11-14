851c16e72e1aecf2941224db193955c9b13f2fb4
James Ferguson <ncjamesferguson@gmail.com>
2017-11-14 16:53:39 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,9 +1,3 @@
-var meals = {};
-var meals2 = new Object();
-var meals3 = { breakfast: "oatmeal" };
-var meals4 = new Object ({ breakfast: "oatmeals"});
-console.log(meals4.breakfast)
-
 var playlist = {
   FleetFoxes: 'Mykonos',
   Bob: 'London Bridge' ,
